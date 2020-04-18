cc7b539183e4a3dd5a6154767b80aa0360afc1b3
Teh Kok How <funcoolgeek@gmail.com>
2020-04-18 13:08:58 +0800

    Improve NumberStringSum by using reverse iterator

--- InterviewQuestions/InterviewQuestions.cpp
+++ InterviewQuestions/InterviewQuestions.cpp
@@ -4620,45 +4620,42 @@ long AddWithoutArithmetic(long sum, long carry)
 // Function for finding sum of larger numbers 
 string NumberStringSum(string& str1, string& str2)
 {
-	// Before proceeding further, make sure length of str2 is larger. 
-	if (str1.length() > str2.length())
-		swap(str1, str2);
+	if (str1.empty() && !str2.empty())
+		return str2;
+	else if (!str1.empty() && str2.empty())
+		return str1;
+	else if (str1.empty() && str2.empty())
+		return "";
 
-	// Take an empty string for storing result 
-	string str = "";
-
+	ostringstream oss;
 	// Calculate length of both string 
 	int n1 = str1.length(), n2 = str2.length();
 	int diff = n2 - n1;
 
 	// Initially take carry zero 
 	int carry = 0;
-
-	// Traverse from end of both strings 
-	for (int i = n1 - 1; i >= 0; i--)
-	{
-		// Do school mathematics, compute sum of 
-		// current digits and carry 
-		int sum = ((str1[i] - '0') +
-			(str2[i + diff] - '0') +
-			carry);
-		str.push_back(sum % 10 + '0');
+	string::reverse_iterator it1 = str1.rbegin();
+	string::reverse_iterator it2 = str2.rbegin();
+	for (; !(it1 == str1.rend() && it2 == str2.rend()); ) {
+		// Do school mathematics, compute sum of current digits and carry
+		int sum = carry;
+		if (it1 != str1.rend())
+			sum += *it1 - '0';
+		if (it2 != str2.rend())
+			sum += *it2 - '0';
+		oss << sum % 10;
 		carry = sum / 10;
+		if (it1 != str1.rend())
+			it1++;
+		if (it2 != str2.rend())
+			it2++;
 	}
-
-	// Add remaining digits of str2[] 
-	for (int i = n2 - n1 - 1; i >= 0; i--)
-	{
-		int sum = ((str2[i] - '0') + carry);
-		str.push_back(sum % 10 + '0');
-		carry = sum / 10;
-	}
-
 	// Add remaining carry 
 	if (carry)
-		str.push_back(carry + '0');
+		oss << carry;
 
-	// reverse resultant string 
+	// reverse resultant string
+	string str = oss.str();
 	reverse(str.begin(), str.end());
 	return str;
 }
