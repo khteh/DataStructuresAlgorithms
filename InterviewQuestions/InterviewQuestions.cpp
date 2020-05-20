e2db18bb9e12db68fcecc58abd4e470eae41037a
Teh Kok How <funcoolgeek@gmail.com>
2020-05-20 10:11:19 +0800

    Clean up URL in comment

--- InterviewQuestions/InterviewQuestions.cpp
+++ InterviewQuestions/InterviewQuestions.cpp
@@ -7617,7 +7617,7 @@ void RotateRightArray(vector<int>& data, int n)
 			}
 		}
 }
-// https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+// https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?
 // 100%
 vector<int> RotateLeftArray(vector<int>& a, int d)
 {
