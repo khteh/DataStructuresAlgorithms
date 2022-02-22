2be59adaff7b7ce0e2e04fe5b218c023b4e2e815
Teh Kok How <kokhow.teh@qashier.com>
2022-02-22 09:07:59 +0800

    Add comment on Tree::ToLinkedList

--- InterviewQuestions/Tree.cpp
+++ InterviewQuestions/Tree.cpp
@@ -838,6 +838,9 @@ void Tree<T>::ToLinkedList()
 	ToLinkedList(m_root);
 }
 /* https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+* Given the root of a binary tree, flatten the tree into a "linked list":
+* The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+* The "linked list" should be in the same order as a pre-order traversal of the binary tree.
 * 100%
 */
 template<typename T>
