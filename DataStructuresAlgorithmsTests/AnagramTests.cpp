8de9fc7905e73515c1753f53b504a936dbfbc73d
Teh Kok How <funcoolgeek@gmail.com>
2022-05-22 15:38:16 +0800

    Split up anagram tests

--- DataStructuresAlgorithmsTests/AnagramTests.cpp
+++ DataStructuresAlgorithmsTests/AnagramTests.cpp
@@ -1,42 +1,46 @@
 #include "pch.h"
 using namespace std;
 TEST(AnagramTests, AnagramTest) {
-  string str, str1;
-  vector<string> strings;
-  str = "Hello World!!!";
-  str1 = "World Hello!!!";
-  ASSERT_TRUE(areAnagrams(str, str1));
-  cout << quoted(str) << " and " << quoted(str1) << " -> " << (areAnagrams(str, str1) ? "Anagram" : "Not anagram") << endl;
-  str1 = "World!!! Hello";
-  ASSERT_TRUE(areAnagrams(str, str1));
-  cout << quoted(str) << " and " << quoted(str1) << " -> " << (areAnagrams(str, str1) ? "Anagram" : "Not anagram") << endl;
-  str1 = "!HWr ole!d!lol";
-  ASSERT_TRUE(areAnagrams(str, str1));
-  cout << quoted(str) << " and " << quoted(str1) << " -> " << (areAnagrams(str, str1) ? "Anagram" : "Not anagram") << endl;
-  str1 = "World Hello!!! ";
-  ASSERT_FALSE(areAnagrams(str, str1));
-  cout << quoted(str) << " and " << quoted(str1) << " -> " << (areAnagrams(str, str1) ? "Anagram" : "Not anagram") << endl;
-  str1 = "world hello!!!";
-  ASSERT_FALSE(areAnagrams(str, str1));
-  cout << quoted(str) << " and " << quoted(str1) << " -> " << (areAnagrams(str, str1) ? "Anagram" : "Not anagram") << endl;
-  str1 = "Good Morning!!!";
-  ASSERT_FALSE(areAnagrams(str, str1));
-  cout << quoted(str) << " and " << quoted(str1) << " -> " << (areAnagrams(str, str1) ? "Anagram" : "Not anagram") << endl;
-  strings.clear();
-  strings.push_back("star"); // "arst" : "star"
-  strings.push_back("dog");  // "dgo" : "dog"
-  strings.push_back("car");  // "acr" : "car"
-  strings.push_back("rats"); // "arst" : rats
-  strings.push_back("arc");  // "acr" : "arc"
-  strings.push_back("god");  // "dgo" : "god"
-  vector<vector<string>> anagrams;
-  ASSERT_EQ(3, FindAnagrams(strings, anagrams));
-  for (vector<vector<string>>::const_iterator it = anagrams.begin(); it != anagrams.end(); it++)
-  {
-	  cout << "( ";
-	  copy(it->begin(), it->end(), ostream_iterator<string>(cout, " "));
-	  cout << "), ";
-  }
+	string str, str1;
+	vector<string> strings;
+	str = "Hello World!!!";
+	str1 = "World Hello!!!";
+	ASSERT_TRUE(areAnagrams(str, str1));
+	cout << quoted(str) << " and " << quoted(str1) << " -> " << (areAnagrams(str, str1) ? "Anagram" : "Not anagram") << endl;
+	str1 = "World!!! Hello";
+	ASSERT_TRUE(areAnagrams(str, str1));
+	cout << quoted(str) << " and " << quoted(str1) << " -> " << (areAnagrams(str, str1) ? "Anagram" : "Not anagram") << endl;
+	str1 = "!HWr ole!d!lol";
+	ASSERT_TRUE(areAnagrams(str, str1));
+	cout << quoted(str) << " and " << quoted(str1) << " -> " << (areAnagrams(str, str1) ? "Anagram" : "Not anagram") << endl;
+	str1 = "World Hello!!! ";
+	ASSERT_FALSE(areAnagrams(str, str1));
+	cout << quoted(str) << " and " << quoted(str1) << " -> " << (areAnagrams(str, str1) ? "Anagram" : "Not anagram") << endl;
+	str1 = "world hello!!!";
+	ASSERT_FALSE(areAnagrams(str, str1));
+	cout << quoted(str) << " and " << quoted(str1) << " -> " << (areAnagrams(str, str1) ? "Anagram" : "Not anagram") << endl;
+	str1 = "Good Morning!!!";
+	ASSERT_FALSE(areAnagrams(str, str1));
+	cout << quoted(str) << " and " << quoted(str1) << " -> " << (areAnagrams(str, str1) ? "Anagram" : "Not anagram") << endl;
+}
+TEST(AnagramTests, FindAnagramsTest) {
+	vector<string> strings;
+	strings.push_back("star"); // "arst" : "star"
+	strings.push_back("dog");  // "dgo" : "dog"
+	strings.push_back("car");  // "acr" : "car"
+	strings.push_back("rats"); // "arst" : rats
+	strings.push_back("arc");  // "acr" : "arc"
+	strings.push_back("god");  // "dgo" : "god"
+	vector<vector<string>> anagrams;
+	ASSERT_EQ(3, FindAnagrams(strings, anagrams));
+	for (vector<vector<string>>::const_iterator it = anagrams.begin(); it != anagrams.end(); it++)
+	{
+		cout << "( ";
+		copy(it->begin(), it->end(), ostream_iterator<string>(cout, " "));
+		cout << "), ";
+	}
+}
+TEST(AnagramTests, sherlockAndAnagramsChallengeTest) {
   ASSERT_EQ(2, sherlockAndAnagrams("mom"));
   ASSERT_EQ(4, sherlockAndAnagrams("abba"));
   ASSERT_EQ(0, sherlockAndAnagrams("abcd"));
