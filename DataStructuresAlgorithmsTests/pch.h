b46a147c22869b63178ed07c43050c114596e04d
Kok How, Teh <funcoolgeek@gmail.com>
2022-07-19 16:04:50 +0800

    Remove unused usings from pch.h

--- DataStructuresAlgorithmsTests/pch.h
+++ DataStructuresAlgorithmsTests/pch.h
@@ -1,14 +1,3 @@
 #pragma once
 #include "gtest/gtest.h"
 #include "../DataStructuresAlgorithms/DataStructuresAlgorithms.h"
-
-using ::testing::AddGlobalTestEnvironment;
-using ::testing::Bool;
-using ::testing::Combine;
-using ::testing::Message;
-using ::testing::Range;
-using ::testing::TestWithParam;
-using ::testing::Values;
-using ::testing::ValuesIn;
-
-using ::testing::internal::ParamGenerator;
