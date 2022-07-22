eff5991a4e3a71d08903788ba152efb0bd0c1d6a
Kok How, Teh <funcoolgeek@gmail.com>
2022-07-22 14:48:02 +0800

    Use TEST_P for ConnectedCellsInAGridTest

--- DataStructuresAlgorithmsTests/DisJointSetTests.cpp
+++ DataStructuresAlgorithmsTests/DisJointSetTests.cpp
@@ -1,7 +1,8 @@
 #include "pch.h"
 using namespace std;
-TEST(DisJointSetTests, DisJointSetTest) {
-	vector<long> a = { 1, 3, 5, 7, 9 };
+TEST(DisJointSetTests, DisJointSetTest)
+{
+	vector<long> a = {1, 3, 5, 7, 9};
 	DisJointSet<long> disjointSet(a);
 	disjointSet.Print(a);
 	// Test islands
@@ -31,7 +32,7 @@ TEST(DisJointSetTests, DisJointSetTest) {
 	ASSERT_EQ(1, disjointSet.Rank(7));
 	disjointSet.Print(a);
 	// Put smaller ranked item under bigger ranked item if ranks are different
-	ASSERT_EQ(7, disjointSet.Union(1, 3));							// 1, 3, 7 root: 7
+	ASSERT_EQ(7, disjointSet.Union(1, 3));							 // 1, 3, 7 root: 7
 	ASSERT_EQ(numeric_limits<long>::min(), disjointSet.Union(1, 7)); // Already joined!
 	ASSERT_EQ(1, disjointSet.Rank(1));
 	ASSERT_EQ(1, disjointSet.Rank(3));
@@ -45,22 +46,33 @@ TEST(DisJointSetTests, DisJointSetTest) {
 	// assert(disjointSet.Find(2) == 0); C++ map will insert non-existing key silently
 	// assert(disjointSet.Find(1) != disjointSet.Find(2)); C++ map will insert non-existing key silently
 }
-TEST(DisJointSetTests, ConnectedCellsInAGridTest) {
-	vector<vector<long>> grid = { {1, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 1, 0}, {1, 0, 0, 0} };
-	ASSERT_EQ(5, ConnectedCellsInAGrid(grid));
+class ConnectedCellsInAGridTestFixture : public testing::TestWithParam<tuple<size_t, vector<vector<long>>>>
+{
+public:
+	void SetUp() override
+	{
+		_expected = get<0>(GetParam());
+		_grid = get<1>(GetParam());
+	}
+	size_t ConnectedCellsInAGridTest()
+	{
+		return ConnectedCellsInAGrid(_grid);
+	}
 
-	grid = { {0, 0, 1, 1}, {0, 0, 1, 0}, {0, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0} };
-	ASSERT_EQ(8, ConnectedCellsInAGrid(grid));
-
-	grid = { {1, 1, 0, 0, 0}, {0, 1, 1, 0, 0}, {0, 0, 1, 0, 1}, {1, 0, 0, 0, 1}, {0, 1, 0, 1, 1} };
-	ASSERT_EQ(5, ConnectedCellsInAGrid(grid));
-
-	grid = { {0, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 1, 0, 1, 0}, {0, 1, 0, 1, 1}, {0, 1, 1, 1, 0} };
-	ASSERT_EQ(15, ConnectedCellsInAGrid(grid));
-
-	grid = { {1, 1, 1, 0, 1}, {0, 0, 1, 0, 0}, {1, 1, 0, 1, 0}, {0, 1, 1, 0, 0}, {0, 0, 0, 0, 0}, {0, 1, 0, 0, 0}, {0, 0, 1, 1, 0} };
-	ASSERT_EQ(9, ConnectedCellsInAGrid(grid));
-
-	grid = { {1, 0, 0, 1, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1}, {1, 0, 1, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 0}, {1, 0, 0, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1}, {0, 1, 0, 0, 0, 1, 0, 0} };
-	ASSERT_EQ(1, ConnectedCellsInAGrid(grid));
+protected:
+	vector<vector<long>> _grid;
+	size_t _expected;
+};
+TEST_P(ConnectedCellsInAGridTestFixture, ConnectedCellsInAGridTests)
+{
+	ASSERT_EQ(this->_expected, this->ConnectedCellsInAGridTest());
 }
+INSTANTIATE_TEST_SUITE_P(
+	DisJointSetTests,
+	ConnectedCellsInAGridTestFixture,
+	::testing::Values(make_tuple(5, vector<vector<long>>{{1, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 1, 0}, {1, 0, 0, 0}}),
+					  make_tuple(8, vector<vector<long>>{{0, 0, 1, 1}, {0, 0, 1, 0}, {0, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}}),
+					  make_tuple(5, vector<vector<long>>{{1, 1, 0, 0, 0}, {0, 1, 1, 0, 0}, {0, 0, 1, 0, 1}, {1, 0, 0, 0, 1}, {0, 1, 0, 1, 1}}),
+					  make_tuple(15, vector<vector<long>>{{0, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 1, 0, 1, 0}, {0, 1, 0, 1, 1}, {0, 1, 1, 1, 0}}),
+					  make_tuple(9, vector<vector<long>>{{1, 1, 1, 0, 1}, {0, 0, 1, 0, 0}, {1, 1, 0, 1, 0}, {0, 1, 1, 0, 0}, {0, 0, 0, 0, 0}, {0, 1, 0, 0, 0}, {0, 0, 1, 1, 0}}),
+					  make_tuple(1, vector<vector<long>>{{1, 0, 0, 1, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1}, {1, 0, 1, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 0}, {1, 0, 0, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1}, {0, 1, 0, 0, 0, 1, 0, 0}})));
