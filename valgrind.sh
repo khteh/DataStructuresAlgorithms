b815a2238cebd6007ae00c8c445a4ca01c5a018e
Kok How, Teh <funcoolgeek@gmail.com>
2024-01-31 17:16:22 +0800

    Update valgrind script

--- valgrind.sh
+++ valgrind.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-valgrind --leak-check=yes --log-file=valgrind.log x64/Debug/DataStructuresAlgorithms
+valgrind --leak-check=yes --log-file=valgrind.log x64/Debug/Console
 echo "Done. Please check valgrind.log"
