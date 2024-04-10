31a0a071d43d3af00bfa66a50fec2b9e5d39d897
Kok How, Teh <funcoolgeek@gmail.com>
2024-04-10 14:54:04 +0800

    Optimize SuffixTreeNode::InsertString

--- src/SuffixTree.cpp
+++ src/SuffixTree.cpp
@@ -207,14 +207,9 @@ void SuffixTreeNode::InsertString(string const &str, size_t index)
 	m_indices.insert(index);
 	if (!str.empty())
 	{
-		map<char, unique_ptr<SuffixTreeNode>>::iterator it = m_children.find(str[0]);
-		if (it != m_children.end())
-			it->second->InsertString(str.substr(1), index);
-		else
-		{
+		if (m_children.find(str[0]) == m_children.end())
 			m_children.emplace(str[0], make_unique<SuffixTreeNode>(str[0]));
-			m_children[str[0]]->InsertString(str.substr(1), index);
-		}
+		m_children[str[0]]->InsertString(str.substr(1), index);
 	}
 }
 void SuffixTreeNode::RemoveString(string const &str)
