91130e2501cca28abb74ca1157c73f5cee6efbc5
Kok How, Teh <funcoolgeek@gmail.com>
2026-03-25 16:07:14 +0800

    Fix application laucnch path config

--- valgrind.sh
+++ valgrind.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-valgrind --leak-check=yes --log-file=valgrind.log x64/Debug/Console
+valgrind --leak-check=yes --log-file=valgrind.log Debug/Console
 echo "Done. Please check valgrind.log"
