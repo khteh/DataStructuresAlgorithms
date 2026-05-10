6f9d849760d12e6caa1b70d653b52ac0fb24866c
Kok How, Teh <funcoolgeek@gmail.com>
2026-05-10 12:58:55 +0800

    Fix missing call to LCSLength before calling LCSBackTrack

--- test/LongestCommonSubsequenceTests.cpp
+++ test/LongestCommonSubsequenceTests.cpp
@@ -28,7 +28,7 @@ INSTANTIATE_TEST_SUITE_P(
 	LongestCommonSubsequenceTestFixture,
 	::testing::Values(make_tuple(2, "HARRY", "SALLY"), make_tuple(3, "SHINCHAN", "NOHARAAA"), make_tuple(2, "ABCDEF", "FBDAMN"), make_tuple(2, "aa", "baaa"), make_tuple(5, "abacba", "abcaba"),
 					  make_tuple(15, "WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHKS", "FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC")));
-class LongestCommonSubsequenceBackTrackTestFixture : public testing::TestWithParam<tuple<string, string, string>>
+class LongestCommonSubsequenceBackTrackTestFixture : public testing::TestWithParam<tuple<tuple<size_t, string>, string, string>>
 {
 public:
 	void SetUp() override
@@ -39,15 +39,17 @@ public:
 		_data1.insert(0, 1, 0);
 		_data2.insert(0, 1, 0);
 	}
-	string LongestCommonSubsequenceBackTrackTest()
+	tuple<size_t, string> LongestCommonSubsequenceBackTrackTest()
 	{
 		vector<vector<size_t>> table(_data1.size(), vector<size_t>(_data2.size())); // Defaults to zero initial value
-		return _lcs.LCSBackTrack(table, _data1, _data2, _data1.size() - 1, _data2.size() - 1);
+		size_t length = _lcs.LCSLength(table, _data1, _data2);
+		string common = _lcs.LCSBackTrack(table, _data1, _data2, _data1.size() - 1, _data2.size() - 1);
+		return make_tuple(length, common);
 	}
 
 protected:
 	LongestCommonSubsequence<char> _lcs;
-	string _expected;
+	tuple<size_t, string> _expected;
 	string _data1, _data2;
 };
 TEST_P(LongestCommonSubsequenceBackTrackTestFixture, LongestCommonSubsequenceBackTrackTests)
@@ -57,8 +59,8 @@ TEST_P(LongestCommonSubsequenceBackTrackTestFixture, LongestCommonSubsequenceBac
 INSTANTIATE_TEST_SUITE_P(
 	LongestCommonSubsequenceBackTrackTests,
 	LongestCommonSubsequenceBackTrackTestFixture,
-	::testing::Values(make_tuple("AY", "HARRY", "SALLY"), make_tuple("NHA", "SHINCHAN", "NOHARAAA"), make_tuple("BD", "ABCDEF", "FBDAMN"), make_tuple("aa", "aa", "baaa"), make_tuple("ababa", "abacba", "abcaba"),
-					  make_tuple("DGCGTRMZJRBAJJV", "WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHKS", "FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC")));
+	::testing::Values(make_tuple(make_tuple(2, "AY"), "HARRY", "SALLY"), make_tuple(make_tuple(3, "NHA"), "SHINCHAN", "NOHARAAA"), make_tuple(make_tuple(2, "BD"), "ABCDEF", "FBDAMN"), make_tuple(make_tuple(2, "aa"), "aa", "baaa"),
+					  make_tuple(make_tuple(5, "ababa"), "abacba", "abcaba"), make_tuple(make_tuple(15, "DGCGTRMZJRBAJJV"), "WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHKS", "FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC")));
 
 class LongestCommonSubsequenceCollectionTestFixture : public testing::TestWithParam<tuple<size_t, vector<size_t>, vector<size_t>>>
 {
@@ -87,7 +89,7 @@ INSTANTIATE_TEST_SUITE_P(
 	LongestCommonSubsequenceCollectionTests,
 	LongestCommonSubsequenceCollectionTestFixture,
 	::testing::Values(make_tuple(3 /*"1 2 3", "1 2 1", "3 4 1" are all correct answers*/, vector<size_t>{1, 2, 3, 4, 1}, vector<size_t>{3, 4, 1, 2, 1, 3})));
-class LongestCommonSubsequenceCollectionBackTrackTestFixture : public testing::TestWithParam<tuple<vector<size_t>, vector<size_t>, vector<size_t>>>
+class LongestCommonSubsequenceCollectionBackTrackTestFixture : public testing::TestWithParam<tuple<tuple<size_t, vector<size_t>>, vector<size_t>, vector<size_t>>>
 {
 public:
 	void SetUp() override
@@ -98,15 +100,17 @@ public:
 		_data1.insert(_data1.begin(), 0);
 		_data2.insert(_data2.begin(), 0);
 	}
-	vector<size_t> LongestCommonSubsequenceCollectionBackTrackTest()
+	tuple<size_t, vector<size_t>> LongestCommonSubsequenceCollectionBackTrackTest()
 	{
 		vector<vector<size_t>> table(_data1.size(), vector<size_t>(_data2.size())); // Defaults to zero initial value
-		return _lcs.LCSBackTrack(table, _data1, _data2, _data1.size() - 1, _data2.size() - 1);
+		size_t length = _lcs.LCSLength(table, _data1, _data2);
+		vector<size_t> common = _lcs.LCSBackTrack(table, _data1, _data2, _data1.size() - 1, _data2.size() - 1);
+		return make_tuple(length, common);
 	}
 
 protected:
 	LongestCommonSubsequence<size_t> _lcs;
-	vector<size_t> _expected;
+	tuple<size_t, vector<size_t>> _expected;
 	vector<size_t> _data1, _data2;
 };
 TEST_P(LongestCommonSubsequenceCollectionBackTrackTestFixture, LongestCommonSubsequenceCollectionBackTrackTests)
@@ -116,4 +120,4 @@ TEST_P(LongestCommonSubsequenceCollectionBackTrackTestFixture, LongestCommonSubs
 INSTANTIATE_TEST_SUITE_P(
 	LongestCommonSubsequenceCollectionBackTrackTests,
 	LongestCommonSubsequenceCollectionBackTrackTestFixture,
-	::testing::Values(make_tuple(vector<size_t>{1, 2, 3} /*"1 2 3", "1 2 1", "3 4 1" are all correct answers*/, vector<size_t>{1, 2, 3, 4, 1}, vector<size_t>{3, 4, 1, 2, 1, 3})));
+	::testing::Values(make_tuple(make_tuple(3, vector<size_t>{1, 2, 3}) /*"1 2 3", "1 2 1", "3 4 1" are all correct answers*/, vector<size_t>{1, 2, 3, 4, 1}, vector<size_t>{3, 4, 1, 2, 1, 3})));
